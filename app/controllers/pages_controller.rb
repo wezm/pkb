782d5416305461d1f85f0b746280f74c1ade9b81
Wesley Moore <wes@wezm.net>
2015-09-28 20:12:48 +1000

    Sort the page index by name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   before_filter :cache_in_varnish, :only => [:index, :show]
 
   def index
-    @pages = Page.all
+    @pages = Page.all.sort { |a,b| a.name <=> b.name }
   end
 
   def show
