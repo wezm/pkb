73299db23de710a2e3f5131ced7fb667478b38b1
Wesley Moore <wes@wezm.net>
2015-11-16 17:17:55 +1100

    Fix broken home page when conditional request is fresh

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,22 +2,21 @@ class PagesController < ApplicationController
 
   def index
     @pages = Page.all.sort { |a,b| a.name <=> b.name }
-    fresh_when(:last_modified => Page.last_modified, :public => true)
     expires_in 1.minute, :public => true
+    fresh_when(:last_modified => Page.last_modified, :public => true)
   end
 
   def show
     @page = Page.new(params[:id])
-    fresh_when(:last_modified => @page.mtime, :public => true)
     expires_in 1.minute, :public => true
+    fresh_when(:last_modified => @page.mtime, :public => true)
   rescue Page::NotFound
     raise ActionController::RoutingError, "Not Found"
   end
 
   def home
     @page = Page.home
-    fresh_when(:last_modified => @page.mtime, :public => true)
     expires_in 1.minute, :public => true
-    render :show
+    fresh_when(:last_modified => @page.mtime, :public => true, :template => 'pages/show')
   end
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,14 +2,14 @@ class TagsController < ApplicationController
 
   def index
     @tags = Tag.all
-    fresh_when(:last_modified => Page.last_modified, :public => true)
     expires_in 1.minute, :public => true
+    fresh_when(:last_modified => Page.last_modified, :public => true)
   end
 
   def show
     @tag = Tag.find(params[:id])
-    fresh_when(:last_modified => @tag.last_modified, :public => true)
     expires_in 1.minute, :public => true
+    fresh_when(:last_modified => @tag.last_modified, :public => true)
   rescue Tag::NotFound
     raise ActionController::RoutingError, "Not Found"
   end
