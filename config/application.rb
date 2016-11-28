9da7186cbb7b3ac927b54f8f7d051ea06598dee9
Wesley Moore <wes@wezm.net>
2016-11-28 12:38:47 +1100

    Fix eager loading on production

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Pkb
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
 
     def settings
       @settings ||= begin
