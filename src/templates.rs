56f9f422948987d6025adabcea4415cfa2125c6f
Wesley Moore <wes@wezm.net>
2022-05-02 14:44:50 +1000

    Fix issues with invalid markup from comrak/kuchiki

--- src/templates.rs
+++ src/templates.rs
@@ -16,3 +16,18 @@ fn markdown(v: &str, adapter: &SyntectAdapter<'_>) -> String {
     plugins.render.codefence_syntax_highlighter = Some(adapter);
     markdown_to_html_with_plugins(v, &options, &plugins)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // This isn't so much a test but documentation that comrak wraps the custom elements in a <p>
+    // tag.
+    #[test]
+    fn test_custom_elements() {
+        let md = "## Recently Updated Pages\n\n<recently-changed-list></recently-changed-list>\n";
+        let adapter = SyntectAdapter::new("base16-ocean.dark");
+        let html = markdown(md, &adapter);
+        assert_eq!(html, "<h2>Recently Updated Pages</h2>\n<p><recently-changed-list></recently-changed-list></p>\n")
+    }
+}
--- src/templates/decorators.rs
+++ src/templates/decorators.rs
@@ -1,7 +1,8 @@
+use std::path::Path;
+
 use html5ever::{LocalName, Namespace, QualName};
 use kuchiki::traits::TendrilSink;
-use kuchiki::{parse_html, Attribute, ExpandedName, NodeRef};
-use std::path::Path;
+use kuchiki::{parse_fragment, Attribute, ExpandedName, NodeRef};
 
 use crate::page::Page;
 use crate::settings::Settings;
@@ -9,15 +10,29 @@ use crate::web;
 
 use crate::string_ext::StringExt;
 
+const START_HTML: &str = "<html>";
+const END_HTML: &str = "</html>";
+
 // Enhance the HTML
 pub fn enhance_markup(html: &str, settings: &Settings) -> String {
-    let doc = parse_html().one(html);
+    let doc = parse_markup(html);
 
     link_headings(&doc);
     process_custom_elements(&doc, &settings.pages_path);
     trim_pre_whitespace(&doc);
 
-    doc.to_string()
+    let mut enhanced_html = doc.to_string();
+    // HACK: The document ends up serialised with a wrapping `<html>` element around the content
+    // strip that here. E.g.
+    // https://github.com/kuchiki-rs/kuchiki/blob/f652e38b12cb0d33f7bb0565b6933a6e2823a0c5/src/tests.rs#L66
+    if enhanced_html.ends_with(END_HTML) {
+        enhanced_html.truncate(enhanced_html.len() - END_HTML.len());
+    }
+    if enhanced_html.starts_with(START_HTML) {
+        enhanced_html[START_HTML.len()..].to_string()
+    } else {
+        enhanced_html
+    }
 }
 
 fn link_headings(doc: &NodeRef) {
@@ -66,7 +81,18 @@ const RECENTLY_MODIFIED_LIMIT: usize = 5;
 
 impl RecentlyChangedList {
     fn process(doc: &NodeRef, basepath: &Path) {
+        let p_qual = QualName::new(None, ns!(html), local_name!("p"));
         for elem in doc.select("recently-changed-list").unwrap() {
+            // comrak wraps the element in <p> tag so we need to replace it to avoid generating
+            // invalid markup
+            let parent = elem.as_node().parent();
+            let node_to_replace = match parent {
+                Some(ref parent) if parent.as_element().map_or(false, |e| e.name == p_qual) => {
+                    parent
+                }
+                Some(_) | None => elem.as_node(),
+            };
+
             let list = NodeRef::new_element(el_name("ul"), []);
             for page in Page::recently_modified(RECENTLY_MODIFIED_LIMIT, basepath) {
                 let li = NodeRef::new_element(el_name("li"), []);
@@ -94,8 +120,8 @@ impl RecentlyChangedList {
             }
 
             // replace elem with the list
-            elem.as_node().insert_after(list);
-            elem.as_node().detach();
+            node_to_replace.insert_after(list);
+            node_to_replace.detach();
         }
     }
 }
@@ -114,14 +140,33 @@ fn el_name(name: &str) -> QualName {
     QualName::new(None, ns!(html), LocalName::from(name))
 }
 
+fn parse_markup(html: &str) -> NodeRef {
+    let ctx_name = QualName::new(None, ns!(html), local_name!("div"));
+    info!("{}", html);
+    parse_fragment(ctx_name.clone(), vec![]).one(html)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use regex::Regex;
+    use rocket::form::validate::Contains;
     use std::path::PathBuf;
 
-    const HTML: &str =
-        "<html><body><h1>Test</h1><recently-changed-list></recently-changed-list></body></html>";
+    const HTML: &str = "<h1>Test</h1><recently-changed-list></recently-changed-list>";
+
+    fn test_settings() -> Settings {
+        Settings {
+            pages_path: pages_path(),
+            author: "Test".to_string(),
+            author_url: "https://example.com/".to_string(),
+            copyright_start_year: 2020,
+            name: "Test Site".to_string(),
+            domain: "example.com".to_string(),
+            tagline: "For testing".to_string(),
+            sentry_dsn: None,
+        }
+    }
 
     fn pages_path() -> PathBuf {
         let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
@@ -132,11 +177,30 @@ mod tests {
     #[test]
     fn recently_changed_list() {
         // Test that it replaces the custom element with a list of pages
-        let doc = parse_html().one(HTML);
+        let doc = parse_markup(HTML);
         RecentlyChangedList::process(&doc, &pages_path());
 
         let processed = doc.to_string();
         let regex = Regex::new(r#"<ul>(<li><a href="/[^"]+">[^<]+</a><span class="smaller-font lighten"> updated <abbr title="\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z">[^<]+</abbr></span></li>)+</ul>"#).unwrap();
         assert!(regex.is_match(&processed));
+    }
+
+    #[test]
+    fn recently_changed_list_strips_parent_p_tag() {
+        // When rendering the Markdown to HTML comrak wraps the custom element in a <p>, test that
+        // this is stripped.
+        let html = "<h2>Recently Updated Pages</h2>\n<p><recently-changed-list></recently-changed-list></p>\n";
+        let doc = parse_markup(html);
+        RecentlyChangedList::process(&doc, &pages_path());
+
+        let processed = doc.to_string();
+        assert!(!processed.contains("<p>"));
+    }
+
+    #[test]
+    fn enhancing_markup_does_not_add_html_tag() {
+        let markup = "<p>no HTML tag please</p>";
+        let enhanced = enhance_markup(markup, &test_settings());
+        assert_eq!(markup, enhanced);
     }
 }
